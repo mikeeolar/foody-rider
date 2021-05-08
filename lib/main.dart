d5b0a68e669f6fda2b0d0ebfbb77cb37e8cd3940
mikeeeola <mikeola758@gmail.com>
2021-05-08 11:08:02 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:foody_rider/helpers/push_notification_service.dart';
 import 'package:foody_rider/screens/dashboard_screen.dart';
 import 'package:foody_rider/screens/login_screen.dart';
 
